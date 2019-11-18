9d442c1936e7be10fd33fb0aae93aa9960ac2332
sokkanen <joel.sokkanen@helsinki.fi>
2019-11-18 15:21:33 +0200

    Initial structure

---
+++ src/test/java/dtt/lukuvinkkikirjasto/demo/LukuvinkkikirjastoApplicationTests.java
@@ -0,0 +1,13 @@
+package dtt.lukuvinkkikirjasto.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LukuvinkkikirjastoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
