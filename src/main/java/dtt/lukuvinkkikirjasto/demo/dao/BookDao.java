c8a661983535d77fad52ef61de02b0e196fe182c
Milla Lintunen <milla.lintunen@hotmail.com>
2019-11-19 13:39:20 +0200

    tietokantayhteys, BookDao alustus, mainClassName build.gradleen

---
+++ src/main/java/dtt/lukuvinkkikirjasto/demo/dao/BookDao.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dtt.lukuvinkkikirjasto.demo.dao;
+
+import dtt.lukuvinkkikirjasto.demo.database.Database;
+
+/**
+ *
+ * @author milla
+ */
+public class BookDao {
+    private final Database database;
+    
+    public BookDao(Database database) {
+        this.database = database;
+        
+    }
+    
+    // add book  
+    // list books
+}
--- src/main/java/dtt/lukuvinkkikirjasto/demo/database/Database.java
+++ src/main/java/dtt/lukuvinkkikirjasto/demo/database/Database.java
@@ -4,25 +4,44 @@ import org.flywaydb.core.Flyway;
 import javax.persistence.Entity;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @Entity
 public class Database {
 
-    private final String url = "jdbc:sqlite:file:./build/lukusuositukset.sql";
+    private final String url;
     private final String userName = "sa";
     private final String password = "";
     private final Flyway flyway;
+    
 
-    public Database(){
-        this.flyway = Flyway.configure().dataSource(url, userName, password).load();
+    public Database(String url){
+        this.url = "jdbc:sqlite:file:"+url;
+        this.flyway = Flyway.configure().dataSource(this.url, userName, password).load();
+        init();
     }
 
     public void doFlyWayMigration(){
         flyway.migrate();
     }
 
-    // TODO: Actual connection to database
-    /*public Connection connect(){
-
-    }*/
+    
+    /**
+     * Opens connection to database
+     * @return connection
+     */
+    public Connection getConnection() throws SQLException {
+        Connection connection = DriverManager.getConnection(url, userName, password);
+        return connection;
+    }
+    
+    public void init() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            getConnection();
+        } catch (Exception e) {
+            System.out.println("Error in database connection");
+        }
+    }
 }
