23cc4cd53d1a321d0631b244ec91a1949bca5dd7
Pasi Alho <alho.pasi@iki.fi>
2019-11-25 14:59:15 +0200

    selenium added, spring server starts on tests, but with wrong db

---
+++ src/test/java/dtt/lukuvinkkikirjasto/demo/ServerRule.java
@@ -0,0 +1,26 @@
+package dtt.lukuvinkkikirjasto.demo;
+
+import org.junit.rules.ExternalResource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+
+class ServerRule extends ExternalResource{
+
+    private final int port;
+    ConfigurableApplicationContext app;
+
+    public ServerRule(int port) {
+        this.port = port;
+    }
+
+    @Override
+    protected void before() throws Throwable {
+        this.app = SpringApplication.run(LukuvinkkikirjastoApplication.class);
+    }
+
+    @Override
+    protected void after() {
+        app.close();
+    }
+}
