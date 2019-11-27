de07b183289e7ffb32c441bea42d9af65e875606
sokkanen <joel.sokkanen@helsinki.fi>
2019-11-28 00:34:32 +0200

    Postgres configuration for Database.

--- src/main/java/dtt/lukuvinkkikirjasto/demo/database/Database.java
+++ src/main/java/dtt/lukuvinkkikirjasto/demo/database/Database.java
@@ -9,33 +9,55 @@ import java.sql.SQLException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class Database {
 
     private final String url;
-    private final String userName = "sa";
-    private final String password = "";
+    private String userName;
+    private String password = "";
     private final Flyway flyway;
+
+    @Autowired
+    private Environment env;
 
     private Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
-    public Database(@Value("./build/lukusuositukset.db") String url){
-        String extension = url;
-        if (System.getProperty("url") != null){
-            extension = System.getProperty("url");
-        }
-        this.url = "jdbc:sqlite:file:" + extension;
+    public Database(@Value("./build/lukusuositukset.db") String url) {
+        boolean inCloud = !getCloudUrl().equals("");
+
+        this.url = inCloud ? getCloudUrl() : localConfig(url);
+        this.userName = inCloud ? env.getProperty("JDBC_DATABASE_USERNAME") : "sa";
+        this.password = inCloud ? env.getProperty("JDBC_DATABASE_PASSWORD") : "";
+
         this.flyway = Flyway.configure().dataSource(this.url, userName, password).load();
-        init();
+        initializeDbConnection(inCloud);
         logger.info("Established database-connection to '{}'", this.url);
         flyway.migrate();
     }
 
     public void doFlyWayMigration(){
         flyway.migrate();
+    }
+
+    private String localConfig(String url){
+        String extension = url;
+        if (System.getProperty("url") != null){
+            extension = System.getProperty("url");
+        }
+        return"jdbc:sqlite:file:" + extension;
+    }
+
+    private String getCloudUrl(){
+        try {
+            return env.getProperty("JDBC_DATABASE_URL");
+        } catch (NullPointerException e){
+            return "";
+        }
     }
     
     /**
@@ -46,13 +68,24 @@ public class Database {
         Connection connection = DriverManager.getConnection(url, userName, password);
         return connection;
     }
-    
-    public void init() {
+
+    /**
+     * Establishes a database-connection.
+     * @param inCloud If run in Heroku --> true, else false
+     */
+    private void initializeDbConnection(boolean inCloud) {
         try {
-            Class.forName("org.sqlite.JDBC");
-            getConnection();
+            if (inCloud){
+                Class.forName("org.postgresql.Driver");
+                logger.info("Connecting to PostgreSQL");
+                getConnection();
+            } else {
+                Class.forName("org.sqlite.JDBC");
+                logger.info("Connecting to SqLite");
+                getConnection();
+            }
         } catch (Exception e) {
-            System.out.println("Error in database connection");
+            logger.warn("Error in database connection: {}", e.getMessage());
         }
     }
 }
