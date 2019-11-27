e46a4aa5cd923ded5ad554d95e2482b4305205eb
Milla Lintunen <milla.lintunen@hotmail.com>
2019-11-27 20:05:04 +0200

    id to book and generated id added to book info

--- src/main/java/dtt/lukuvinkkikirjasto/demo/domain/Book.java
+++ src/main/java/dtt/lukuvinkkikirjasto/demo/domain/Book.java
@@ -12,7 +12,9 @@ import javax.validation.constraints.Size;
  * @author milla
  */
 public class Book {
+
     
+    private int id;
     @Size(min = 2, max = 30, message="Title must be between 2 and 30 characters")
     private String title;
 
@@ -61,6 +63,15 @@ public class Book {
 
     public void setIsbn(String isbn) {
         this.isbn = isbn;
+    }
+    
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
 }
