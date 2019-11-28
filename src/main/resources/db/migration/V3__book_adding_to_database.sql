c0873d5b11e93e406860f2be31819b93c53e204f
sokkanen <joel.sokkanen@helsinki.fi>
2019-11-28 23:33:20 +0200

    Quotation

--- src/main/resources/db/migration/V3__book_adding_to_database.sql
+++ src/main/resources/db/migration/V3__book_adding_to_database.sql
@@ -1,2 +1,2 @@
-INSERT INTO book (title, author, isbn, read_already) values ("Kellomies", "Joona Vuorenpää","9789513161699", false);
-INSERT INTO book (title, author, isbn, read_already) values ("Idiootit ympärilläni", "Thomas Erikson","9789523003217", false);
+INSERT INTO book (title, author, isbn, read_already) VALUES ('Kellomies', 'Joona Vuorenpää','9789513161699', false);
+INSERT INTO book (title, author, isbn, read_already) VALUES ('Idiootit ympärilläni', 'Thomas Erikson','9789523003217', false);
