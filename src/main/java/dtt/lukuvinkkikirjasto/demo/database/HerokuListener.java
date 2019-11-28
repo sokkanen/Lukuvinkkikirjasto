656c9e90adcf074146693b950b039549c18610e3
sokkanen <joel.sokkanen@helsinki.fi>
2019-11-28 18:01:59 +0200

    Environment listener for Database connection

---
+++ src/main/java/dtt/lukuvinkkikirjasto/demo/database/HerokuListener.java
@@ -0,0 +1,35 @@
+package dtt.lukuvinkkikirjasto.demo.database;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.core.env.ConfigurableEnvironment;
+import java.lang.invoke.MethodHandles;
+public class HerokuListener implements ApplicationListener<ApplicationEnvironmentPreparedEvent> {
+    private Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    @Override
+    public void onApplicationEvent(ApplicationEnvironmentPreparedEvent event) {
+        ConfigurableEnvironment env = event.getEnvironment();
+        boolean inCloud = env.getProperty("DYNO") != null ? setHerokuProperties(env) : setLocalProperties(env);
+        if (inCloud){
+            logger.info("Properties for Cloud environment set.");
+        } else {
+            logger.info("Properties for Local environment set");
+        }
+    }
+
+    private boolean setHerokuProperties(ConfigurableEnvironment env){
+        logger.info("Found HEROKU environment. Setting System properties.");
+        System.setProperty("JDBC_DATABASE_URL", env.getProperty("JDBC_DATABASE_URL"));
+        System.setProperty("JDBC_DATABASE_USERNAME", env.getProperty("JDBC_DATABASE_USERNAME"));
+        System.setProperty("JDBC_DATABASE_PASSWORD", env.getProperty("JDBC_DATABASE_PASSWORD"));
+        return true;
+    }
+
+    private boolean setLocalProperties(ConfigurableEnvironment env){
+        System.setProperty("JDBC_DATABASE_URL", "./build/lukusuositukset.db");
+        System.setProperty("JDBC_DATABASE_USERNAME", "sa");
+        System.setProperty("JDBC_DATABASE_PASSWORD", "");
+        return false;
+    }
+}
