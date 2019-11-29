89ea470d59ac52b99a6528cebe03f43db9c3c3bc
Joel Sokkanen <joel.sokkanen@veikkaus.fi>
2019-11-29 10:56:12 +0200

    Fix for Circle

--- src/main/java/dtt/lukuvinkkikirjasto/demo/database/Database.java
+++ src/main/java/dtt/lukuvinkkikirjasto/demo/database/Database.java
@@ -19,6 +19,7 @@ public class Database {
     private final String userName;
     private final String password;
     private final Flyway flyway;
+    private final String defaultTestDbForCircle = "jdbc:sqlite:file:./build/lukuvinkkitest.db";
 
     private Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
@@ -26,6 +27,9 @@ public class Database {
         this.url = System.getProperty("JDBC_DATABASE_URL");
         this.userName = System.getProperty("JDBC_DATABASE_USERNAME");
         this.password = System.getProperty("JDBC_DATABASE_PASSWORD");
+        if (this.url == null) {
+            this.url = defaultTestDbForCircle;
+        }
         this.flyway = Flyway.configure().dataSource(this.url, userName, password).load();
         boolean usingLocally = this.url.contains("build");
         initializeDbConnection(usingLocally);
@@ -33,7 +37,7 @@ public class Database {
         doFlyWayMigration();
     }
 
-    public void doFlyWayMigration(){
+    public void doFlyWayMigration() {
         logger.info("FLYWAY URL: {}", this.url);
         flyway.migrate();
     }
